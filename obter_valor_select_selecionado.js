6b18b7ad40d676a5f8ac3cb1f8728ee0d364bf99
cjatoba <52887959+cjatoba@users.noreply.github.com>
2022-01-20 10:01:25 -0300

    Delete obter_valor_select_selecionado.js

--- obter_valor_select_selecionado.js
+++
@@ -1,7 +0,0 @@
-const select = document.querySelector('#id_select')
-select.options[select.selectedIndex].value
-
-var opt = document.createElement('option');
-opt.value = "Value";
-opt.innerHTML = "Value";
-select.appendChild(opt); 
