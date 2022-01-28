3e51af2179ca153efdbf781e7e09371c69b7562d
cjatoba <52887959+cjatoba@users.noreply.github.com>
2022-01-28 14:53:33 -0300

    Create index.js

---
+++ Manipular eventos/index.js
@@ -0,0 +1,23 @@
+//Verifica se o DOM carregou
+document.addEventListener("DOMContentLoaded", function(event) { 
+// carregou o DOM
+  
+  //Manipula o evento click
+  element.onclick = (evt) => {};
+  
+  //Manipula o evento change
+  element.onchange = (evt) => {};
+  
+  //Manipula o evento focus
+  element.onfocus = (evt) => {};
+  
+  //Manipula o evento mouse enter
+  element.onmouseenter = (evt) => {};
+  
+  //Manipula o evento key press
+  element.onkeypress = (evt) => {};
+  
+  //Manipula o evento submit
+  element.onsubmit = (evt) => {};
+})
+
