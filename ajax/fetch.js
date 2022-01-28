269d241aad6b610eb7ba9532f82697d03e13e080
cjatoba <52887959+cjatoba@users.noreply.github.com>
2022-01-28 14:57:02 -0300

    Update fetch.js

--- ajax/fetch.js
+++ ajax/fetch.js
@@ -5,13 +5,18 @@ let data_form = new FormData(form_selected);
 data_form.append("key_name", "value");
 data_form.append("key_name2", "value");
 
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
 fetch("page.php", {
+    headers: myHeaders,
     method: "POST",
+    mode: 'cors',
     body: data_form
 })
-.then(response => response.json())
+.then(response => response.json()) // response.blob() para binários; response.text() para texto; response.json() para JSON;
 .then(responseJson => {
-    
+    //Retorno da requisição no formato especificado no then anterior (Ocorrerá um erro caso o retorno seja diferente do esperado)
 })
 .catch(error => {
     console.log("Erro na requisição: " + fail);
